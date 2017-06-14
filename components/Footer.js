42a99d5218300bcab3eadb9a12635e8236e241ec
jkupcho <jonathan.kupcho@gmail.com>
2017-06-13 23:22:39 -0500

    added about us

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import ContactForm from '../containers/ContactForm'
 
 export default () => {
   return (
-    <footer className="footer">
+    <footer className="footer" style={{borderTop: '1px solid #cfcfcf'}}>
       <div className="container">
         <div className="content has-text-centered">
           <Title size={1}>Contact Us</Title>
@@ -23,6 +23,14 @@ export default () => {
                   1505 Sherwood Rd.<br/> 
                   Shoreview MN, 55126<br/> 
                 </address>
+              </div>
+            </div>
+            <div className="columns">
+              <div className="column is-one-quarter has-text-right">
+                <Icon name="phone" />
+              </div>
+              <div className="column">
+                <a href="tel:651-786-6607">651-786-6607</a>
               </div>
             </div>
           </div>
